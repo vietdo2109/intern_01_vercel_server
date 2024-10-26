3be10c46e19681a2df959beeff42d36d2862f6fc
dovanviet <dovanviet2109@gmail.com>
2024-10-26 12:37:43 +0700

    new server

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
-// JSON Server module
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const fs = require("fs");
+const cors = require("cors");
 const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
-// Make sure the db.json is only read-only and served from memory to avoid errors when deployed to vercel.
-const data = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "db.json"), "utf-8")
-);
-const router = jsonServer.router(data);
 
+server.use(cors());
+server.use(jsonServer.bodyParser);
 server.use(middlewares);
-
-server.use(
-  // Add custom route here if needed
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
 server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running");
-});
 
-// Export the Server API
-module.exports = server;
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
